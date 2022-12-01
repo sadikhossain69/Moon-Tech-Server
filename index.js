fb94c673776be729fa3d7bfdfb6d59c8cacb55b5
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-01 18:08:40 +0600

    database issue founded

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ff1bu0r.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://sadik:ddJQTx7jw1AvQ0HL@cluster0.ff1bu0r.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
